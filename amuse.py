2f9155a55e9b1abbc514282ab8c454619e845195
Priya Raghunathan <priya@amuse.io>
2013-06-02 22:52:00 +0100

    add a third joke

--- amuse.py
+++ amuse.py
@@ -3,6 +3,8 @@ import random
 JOKES = [
     "Why did the chicken cross the road? To get to the other side.",
     "I would tell you a UDP joke, but you might not get it.",
+    "There are two hard problems in computer science: cache invalidation, "
+    "naming things, and off-by-one errors.",
 ]
 
 
