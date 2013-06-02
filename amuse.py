a7085fbb44291b6cc93b90590b692ec8131e0acf
Priya Raghunathan <priya@amuse.io>
2013-06-02 22:40:00 +0100

    add a second joke

--- amuse.py
+++ amuse.py
@@ -1,5 +1,13 @@
+import random
+
+JOKES = [
+    "Why did the chicken cross the road? To get to the other side.",
+    "I would tell you a UDP joke, but you might not get it.",
+]
+
+
 def amuse():
-    return "Why did the chicken cross the road? To get to the other side."
+    return random.choice(JOKES)
 
 
 if __name__ == "__main__":
